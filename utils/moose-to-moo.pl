a7990f4ca179a51d3b140bf5cdadb525594a5dba
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2018-11-28 12:56:26 +0100

    migration script: use lazy comment
    See d7529e9 ("Comment those 'is' that are for 'lazy_build'",
    2018-11-28).

--- utils/moose-to-moo.pl
+++ utils/moose-to-moo.pl
@@ -2,7 +2,7 @@ use v5.10.0;
 use strict;
 use warnings;
 
-# Usage: git reset --hard; perl utils/moose-to-moo.pl $(git ls-files ':!utils/moose-to-moo.pl'); prove -Ilib -j8 -r t
+# Usage: git checkout -f lib t; perl utils/moose-to-moo.pl $(git ls-files ':!utils/moose-to-moo.pl'); prove -Ilib -j8 -r t
 
 BEGIN { $^I = ""; }
 LINE: while (defined($_ = readline ARGV)) {
@@ -12,6 +12,9 @@ LINE: while (defined($_ = readline ARGV)) {
     s/use MooseX::StrictConstructor;/use MooX::StrictConstructor;/g;
     s/__PACKAGE__->meta->make_immutable;/1;/g;
     s/use namespace::clean -except => 'meta';/use namespace::clean -except => 'new';/g;
+
+    s/^\s+is\s+=> 'ro', # for lazy_build\n//g;
+    s/^(\s+)lazy_build(\s+)=> 1,\n/${1}is        $2=> 'lazy',\n${1}predicate $2=> 1,\n${1}clearer   $2=> 1,\n/g;
 }
 continue {
     die "-p destination: $!\n" unless print $_;
